481b59ae724902e8ec5735bf0c6e43b28293cfd4
Gerard <gespanadiez@gmail.com>
2025-08-26 02:54:59 +0200

    algunos archivos modificados, VITE y vite-project

---
+++ proyectos/vite-project/src/main.js
@@ -0,0 +1,6 @@
+import './css/normalize.css'; // normalize.css: unifica estilos por defecto entre navegadores para que los elementos se vean similares
+import './css/estilos.css';
+import './css/variables.css';
+import './css/header.css';
+import './css/hero.css';
+
