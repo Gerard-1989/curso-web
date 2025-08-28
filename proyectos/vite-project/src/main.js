529c5c9b8180c5fe56ca5eaace759e883163497e
Gerard <gespanadiez@gmail.com>
2025-08-29 01:03:48 +0200

    caracteristicas html y css terminado

--- proyectos/vite-project/src/main.js
+++ proyectos/vite-project/src/main.js
@@ -1,6 +1,11 @@
+// Archivo de entrada de todos nuestros estilos
+
 import './css/normalize.css'; // normalize.css: unifica estilos por defecto entre navegadores para que los elementos se vean similares
-import './css/estilos.css';
+import './css/estilos-generales.css';
 import './css/variables.css';
 import './css/header.css';
 import './css/hero.css';
+import './css/caracteristicas.css'
+
+
 
